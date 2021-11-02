e497e521bc4212724b1948f56a318f9d922dd2e4
Laksh <ssglaksh17@gmail.com>
2021-11-02 17:04:59 +0530

    -> Adjusted URLs

--- src/components/Section.js
+++ src/components/Section.js
@@ -25,7 +25,7 @@ function Section(props) {
                         }
                     </ButtonGroup>
                 </Fade>
-                <DownArrow src="/images/down-arrow.svg" />
+                <DownArrow src="./images/down-arrow.svg" />
             </Buttons>
         </Wrap>
     )
